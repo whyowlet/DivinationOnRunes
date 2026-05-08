4ca7965c0ebf629adbc4384f16c4c1916d6bb8ed
Sasha Scherbinin <sswssh@icloud.com>
2026-05-09 00:50:35 +0500

    Add Rune class

--- DivinationOnRunes.Core/Rune.cs
+++ DivinationOnRunes.Core/Rune.cs
@@ -1,5 +1,38 @@
 ﻿namespace DivinationOnRunes.Core;
 
+/// <summary>
+/// Описывает одну руну Старшего Футарка
+/// </summary>
 public class Rune
 {
+    // Уникальный идентификатор руны
+    public int Id { get; }
+    
+    // Название руны на русском языке
+    public string Name { get; }
+    
+    // Название руны на английском языке
+    public string EnglishName { get; }
+    
+    // Толкование в прямом положении
+    public string UprightMeaning { get; }
+    
+    // Толкование в перевёрнутом положении
+    public string ReversedMeaning { get; }
+    
+    // Может ли руна выпасть в перевёрнутом положении
+    public bool IsReversible { get; }
+
+    public Rune(int id, string name, string englishName,
+        string uprightMeaning, string reversedMeaning, bool isReversible)
+    {
+        Id = id;
+        Name = name;
+        EnglishName = englishName;
+        UprightMeaning = uprightMeaning;
+        ReversedMeaning = reversedMeaning;
+        IsReversible = isReversible;
+    }
+    
+    public override string ToString() => $"{Name} ({EnglishName})";
 }
